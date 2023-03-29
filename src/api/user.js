b67bff2e661d2f0da3e56cf8d141a61fa8940b24
ggkk0818 <ggkk0818@sina.com>
2023-03-29 23:52:18 +0800

    ✨ feat(系统管理): 完善用户新增编辑功能

--- src/api/user.js
+++ src/api/user.js
@@ -1,14 +1,49 @@
 import request from '@/utils/request'
 /**
  * 查询用户列表
- * @param {*} paras
+ * @param {*} params
  * @returns
  */
-export function getUserList (paras) {
+export function getUserList (params) {
   return request({
     url: 'admin',
     method: 'get',
-    params: paras
+    params: params
+  })
+}
+/**
+ * 查询用户详情
+ * @param {*} id
+ * @returns
+ */
+export function getUserDetail (id) {
+  return request({
+    url: `admin/${encodeURIComponent(id)}`,
+    method: 'get'
+  })
+}
+/**
+ * 新增用户
+ * @param {*} params
+ * @returns
+ */
+export function addUser (params) {
+  return request({
+    url: 'admin',
+    method: 'post',
+    data: params
+  })
+}
+/**
+ * 更新用户
+ * @param {*} params
+ * @returns
+ */
+export function updateUser (params) {
+  return request({
+    url: 'admin',
+    method: 'put',
+    data: params
   })
 }
 /**
--- src/views/system/user/edit.vue
+++ src/views/system/user/edit.vue
@@ -3,6 +3,7 @@
   <page-header-wrapper :title="isEdit ? '编辑用户' : '新增用户'">
     <a-card :body-style="{ padding: '24px 32px' }" :bordered="false">
       <a-form-model
+        ref="form"
         :model="formData"
         :rules="formRules"
         :label-col="{ lg: { span: 7 }, sm: { span: 7 } }"
@@ -14,6 +15,18 @@
         <a-form-model-item label="密码" prop="password">
           <a-input-password v-model="formData.password" placeholder="请输入密码" />
         </a-form-model-item>
+        <a-form-model-item label="昵称" prop="nickName">
+          <a-input v-model="formData.nickName" placeholder="请输入昵称" />
+        </a-form-model-item>
+        <a-form-model-item label="邮箱" prop="email">
+          <a-input v-model="formData.email" placeholder="请输入邮箱" />
+        </a-form-model-item>
+        <a-form-model-item label="手机号" prop="mobile">
+          <a-input v-model="formData.mobile" placeholder="请输入手机号" />
+        </a-form-model-item>
+        <a-form-model-item label="备注" prop="remark">
+          <a-input v-model="formData.remark" placeholder="请输入备注" />
+        </a-form-model-item>
         <a-form-model-item :wrapperCol="{ span: 24 }" style="text-align: center">
           <a-button type="primary" @click="onSubmit">提交</a-button>
           <a-button style="margin-left: 8px">重置</a-button>
@@ -24,11 +37,22 @@
 </template>
 
 <script>
+import { addUser, updateUser, getUserDetail } from '@/api/user'
+const DEFAULT_VALUES = {
+  id: null,
+  name: '',
+  password: '',
+  nickName: '',
+  email: '',
+  mobile: '',
+  remark: ''
+}
 export default {
-  name: 'BaseForm',
+  name: 'UserEdit',
   data () {
     return {
-      formData: {},
+      isLoading: false,
+      formData: { ...DEFAULT_VALUES },
       formRules: {
         name: [{ required: true, message: '请输入用户名' }]
       },
@@ -36,20 +60,61 @@ export default {
     }
   },
   computed: {
-    isEdit ({ detailData }) {
-      return !!detailData
+    isEdit ({ $route }) {
+      return !!$route.params.id
+    }
+  },
+  watch: {
+    $route: {
+      handler () {
+        this.init()
+      },
+      immediate: true
     }
   },
   methods: {
+    async init () {
+      this.formData = { ...DEFAULT_VALUES }
+      if (this.isEdit) {
+        this.isLoading = true
+        try {
+          const data = await getUserDetail(this.$route.params.id)
+          this.detailData = data || null
+          if (data) {
+            Object.keys(DEFAULT_VALUES).forEach((key) => {
+              if (data[key] != null) {
+                this.formData[key] = data[key]
+              }
+            })
+          }
+        } finally {
+          this.isLoading = false
+        }
+      }
+    },
     onSubmit (e) {
-      this.$refs.ruleForm.validate((valid) => {
+      this.$refs.form.validate(async (valid) => {
         if (valid) {
-          alert('submit!')
+          this.isLoading = true
+          try {
+            if (this.isEdit) {
+              await updateUser({ ...this.detailData, ...this.formData })
+            } else {
+              await addUser(this.formData)
+            }
+            this.$message.success('操作成功')
+            this.$router.back()
+          } finally {
+            this.isLoading = false
+          }
         } else {
           console.log('error submit!!')
           return false
         }
       })
+    },
+    resetForm () {
+      this.$refs.form.resetFields()
     }
   }
 }
