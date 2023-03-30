9871efd07ca0f6ddedbd5f43b13f9173d56046f6
ggkk0818 <ggkk0818@sina.com>
2023-03-30 22:10:08 +0800

    ✨ feat(通用): 调整系统名称

--- src/layouts/UserLayout.vue
+++ src/layouts/UserLayout.vue
@@ -9,18 +9,18 @@
         <div class="top">
           <div class="header">
             <a href="/">
-              <img src="~@/assets/logo.svg" class="logo" alt="logo">
-              <span class="title">Ant Design</span>
+              <!-- <img src="~@/assets/logo.svg" class="logo" alt="logo"> -->
+              <span class="title">{{ systemName }}</span>
             </a>
           </div>
-          <div class="desc">
+          <!-- <div class="desc">
             {{ $t('layouts.userLayout.title') }}
-          </div>
+          </div> -->
         </div>
 
         <router-view />
 
-        <div class="footer">
+        <!-- <div class="footer">
           <div class="links">
             <a href="_self">帮助</a>
             <a href="_self">隐私</a>
@@ -29,7 +29,7 @@
           <div class="copyright">
             Copyright &copy; 2018 vueComponent
           </div>
-        </div>
+        </div> -->
       </div>
     </div>
   </div>
@@ -38,13 +38,18 @@
 <script>
 import { deviceMixin } from '@/store/device-mixin'
 import SelectLang from '@/components/SelectLang'
-
+import defaultSettings from '@/config/defaultSettings'
 export default {
   name: 'UserLayout',
   components: {
     SelectLang
   },
   mixins: [deviceMixin],
+  data () {
+    return {
+      systemName: defaultSettings.title || ''
+    }
+  },
   mounted () {
     document.body.classList.add('userLayout')
   },
